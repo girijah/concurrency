9e51bd21eeb8280dbcbfc9aeb30c55214cb65098
girijah <girijah@live.com>
2017-12-12 18:57:08 +0530

    Volatile

---
+++ src/Multithreading_Cache_Volatile/VolatileTester.java
@@ -0,0 +1,87 @@
+package Multithreading_Cache_Volatile;
+
+/**
+ * Created by girijah on 12/12/2017.
+ */
+public class VolatileTester {
+
+    public static void main (String[] args) {
+
+        // Volatile working or not cannot be  debugged properly - in fact that's the nature of concurrency
+        // that it is not sequence and it's unpredictable
+
+        // Here name can output null if you run that doesn't mean though volatile variable is defined, other threads not getting it's value
+        // Nevertheless because the thread is already terminated by the time other name set thread start executing. This doesn't mean volatile
+        // didn't do its task/job!
+
+        Thread t1= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Output: "+Singleton.getInstance());
+                Singleton.setObjName("Meow!!");
+                System.out.println("t1 Name: "+Singleton.getName());
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.println("Output: "+Singleton.getInstance());
+                System.out.println("t2 Name: "+Singleton.getName());
+
+            }
+        });
+
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.println("Output: "+Singleton.getInstance());
+                Singleton.setObjName("WolWol");
+                System.out.println("t3 Name: "+Singleton.getName());
+
+            }
+        });
+
+        Thread t4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                System.out.println("Output: "+Singleton.getInstance());
+                System.out.println("t4 Name: "+Singleton.getName());
+
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
+}
+
+ class Singleton {
+
+    private volatile static Singleton _instance; //volatile variable
+    private volatile static String objName;
+
+    public static Singleton getInstance() {
+        if (_instance == null) {
+            synchronized (Singleton.class) {
+                if (_instance == null)
+                    _instance = new Singleton();
+            }
+        }
+        return _instance;
+    }
+
+     public static String getName() {
+         return objName;
+     }
+
+     public static void setObjName(String objectName) {
+         objName = objectName;
+     }
+
+}
