fa23574cf128ee4642f5caeadab3c51d0c76fb01
girijah <girijah@live.com>
2017-12-12 08:17:34 +0530

    Volatile

---
+++ src/Multithreading_Cache_Volatile/Executor.java
@@ -0,0 +1,52 @@
+package Multithreading_Cache_Volatile;
+
+/**
+ * Created by girijah on 12/11/2017.
+ */
+public class Executor {
+
+    private static volatile boolean running = true;
+    // Couldn't see any cache problem in this example. However, since multiple threads try
+    // accessing the same variable/resource, there is possible the variable not get updated. So, prevent it by using the keyword Volatile
+
+    public static void main(String[] args) {
+
+        for (int i=0; i<1000; i++) {
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (running) {
+                        System.out.println("running "+Thread.currentThread().getName());
+                    }
+                }
+            }).start();
+
+        }
+
+//        try {
+//            Thread.sleep(100);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for(int i=0; i<10001; i++) {
+                   running = true;
+                    if(i==10000) {
+                        running = false;
+                    }
+                }
+
+            }
+        }).start();
+
+
+        System.out.println("Process finished with the last line executed..................!");
+
+    }
+
+}
--- src/ThreadVsRunnable/ThreadVsRunnable.java
+++ src/ThreadVsRunnable/ThreadVsRunnable.java
@@ -33,22 +33,22 @@ public class ThreadVsRunnable {
         System.out.println("Thread state: " + t1.getState());
         t1.start();
         System.out.println("Thread state: " + t1.getState());
-        Thread.sleep(1000); // Waiting for 1 second before starting next thread
+        Thread.sleep(1000);                                 // Waiting for 1 second before executing next line
         System.out.println("Thread state: " + t1.getState());
 
         Thread t2 = new Thread(rc);
         t2.start();
-        Thread.sleep(1000); // Waiting for 1 second before starting next thread
+        Thread.sleep(1000);                                 // Waiting for 1 second before executing next line
         Thread t3 = new Thread(rc);
         t3.start();
 
         //Creating new instance for every thread access.
         ExtendsThread tc1 = new ExtendsThread();
         tc1.start();
-        Thread.sleep(1000); // Waiting for 1 second before starting next thread
+        Thread.sleep(1000);                                 // Waiting for 1 second before executing next line
         ExtendsThread tc2 = new ExtendsThread();
         tc2.start();
-        Thread.sleep(1000); // Waiting for 1 second before starting next thread
+        Thread.sleep(1000);                                 // Waiting for 1 second before executing next line
         ExtendsThread tc3 = new ExtendsThread();
         tc3.start();
     }
